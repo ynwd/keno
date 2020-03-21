2046a4845516962b087bd73f26508fc8736b94f1
ynwd <ynwd@users.noreply.github.com>
2020-03-22 04:17:37 +0700

    add @babel/plugin-transform-runtime

--- src/webpack.config.ts
+++ src/webpack.config.ts
@@ -59,7 +59,8 @@ const config: Configuration = {
               // plugin-proposal-decorators is only needed if you're using experimental decorators in TypeScript
               ['@babel/plugin-proposal-decorators', { legacy: true }],
               ['@babel/plugin-proposal-class-properties', { loose: true }],
-              'react-hot-loader/babel'
+              'react-hot-loader/babel',
+              '@babel/transform-runtime'
             ]
           }
         }
